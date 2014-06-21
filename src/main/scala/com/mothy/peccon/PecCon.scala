f0757920195f8d51a7603f98c75b741c9c9d80ac
moth1313 <mothy1313@gmail.com>
2014-06-21 20:04:20 +0100

    a
    Initial structure and mod core file

---
+++ src/main/scala/com/mothy/peccon/PecCon.scala
@@ -0,0 +1,29 @@
+package com.mothy.peccon.PecCon
+
+import cpw.mods.fml.common.Mod
+import cpw.mods.fml.common.Mod.EventHandler
+import cpw.mods.fml.common.event.{FMLPostInitializationEvent, FMLPreInitializationEvent, FMLInitializationEvent}
+
+
+@Mod(modid = "peccon", name = "Peculiar Contraptions", version = "1.7.2-0.0.1", modLanguage = "scala")
+object PecCon {
+
+  @EventHandler
+  def preInit(event: FMLPreInitializationEvent){
+
+
+  }
+
+  @EventHandler
+  def init(event: FMLInitializationEvent) {
+
+
+  }
+
+  @EventHandler
+  def postInit(event: FMLPostInitializationEvent){
+
+
+  }
+
+}
